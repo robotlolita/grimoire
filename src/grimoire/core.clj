b3b21911e54e2e989c0dda59c7afabc4ba14f2ad
Quildreen "Sorella" Motta <quildreen@gmail.com>
2013-12-06 13:56:00 -0200

    ...

--- src/grimoire/core.clj
+++
@@ -1,6 +0,0 @@
-(ns grimoire.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/grimoire/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns grimoire.core-test
-  (:require [clojure.test :refer :all]
-            [grimoire.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
